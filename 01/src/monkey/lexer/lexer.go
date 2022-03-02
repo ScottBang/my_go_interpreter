d2879e36a40cffce6eddaaed7d1c840fa4194473
Scottbang <45381235+ScottBang@users.noreply.github.com>
2022-03-02 15:45:39 +0900

    2022.03.02 15:45

--- 01/src/monkey/lexer/lexer.go
+++ 01/src/monkey/lexer/lexer.go
@@ -3,19 +3,21 @@ package lexer
 import "monkey01/token"
 
 type Lexer struct {
-	input        string
-	position     int
-	readPosition int
-	ch           byte
+	input        string // full string
+	position     int    // current char position
+	readPosition int    // next char position
+	ch           byte   // current char
 }
 
 func New(input string) *Lexer {
+	// input 문자열로 Lexer 객체를 초기화 한다.
 	l := &Lexer{input: input}
 	l.readChar()
 	return l
 }
 
 func (l *Lexer) readChar() {
+	// Lexer.ch 에 Lexer.position 이 가리키고 있는 문자를 지정한다.
 	if l.readPosition >= len(l.input) {
 		l.ch = 0
 	} else {
@@ -26,68 +28,31 @@ func (l *Lexer) readChar() {
 }
 
 func (l *Lexer) NextToken() token.Token {
+	//
 	var tok token.Token
 
 	l.skipWhitespace()
 
 	switch l.ch {
 	case '=':
-		if l.peekChar() == '=' {
-			ch := l.ch
-			l.readChar()
-			literal := string(ch) + string(l.ch)
-			tok = token.Token{Type: token.EQ, Literal: literal}
-		} else {
-			tok = newToken(token.ASSIGN, l.ch)
-		}
-	case '+':
-		tok = newToken(token.PLUS, l.ch)
-	case '-':
-		tok = newToken(token.MINUS, l.ch)
-	case '!':
-		if l.peekChar() == '=' {
-			ch := l.ch
-			l.readChar()
-			literal := string(ch) + string(l.ch)
-			tok = token.Token{Type: token.NOT_EQ, Literal: literal}
-		} else {
-			tok = newToken(token.BANG, l.ch)
-		}
-	case '/':
-		tok = newToken(token.SLASH, l.ch)
-	case '*':
-		tok = newToken(token.ASTERISK, l.ch)
-	case '<':
-		tok = newToken(token.LT, l.ch)
-	case '>':
-		tok = newToken(token.GT, l.ch)
+		tok = newToken(token.ASSIGN, l.ch)
 	case ';':
 		tok = newToken(token.SEMICOLON, l.ch)
-	case ',':
-		tok = newToken(token.COMMA, l.ch)
-	case '{':
-		tok = newToken(token.LBRACE, l.ch)
-	case '}':
-		tok = newToken(token.RBRACE, l.ch)
 	case '(':
 		tok = newToken(token.LPAREN, l.ch)
 	case ')':
 		tok = newToken(token.RPAREN, l.ch)
+	case ',':
+		tok = newToken(token.COMMA, l.ch)
+	case '+':
+		tok = newToken(token.PLUS, l.ch)
+	case '{':
+		tok = newToken(token.LBRACE, l.ch)
+	case '}':
+		tok = newToken(token.RBRACE, l.ch)
 	case 0:
 		tok.Literal = ""
 		tok.Type = token.EOF
-	default:
-		if isLetter(l.ch) {
-			tok.Literal = l.readIdentifier()
-			tok.Type = token.LookupIdent(tok.Literal)
-			return tok
-		} else if isDigit(l.ch) {
-			tok.Type = token.INT
-			tok.Literal = l.readNumber()
-			return tok
-		} else {
-			tok = newToken(token.ILLEGAL, l.ch)
-		}
 	}
 
 	l.readChar()
@@ -95,16 +60,9 @@ func (l *Lexer) NextToken() token.Token {
 }
 
 func (l *Lexer) skipWhitespace() {
+	// 빈 문자를 skip 하는 처리.
 	for l.ch == ' ' || l.ch == '\t' || l.ch == '\n' || l.ch == '\r' {
 		l.readChar()
-	}
-}
-
-func (l *Lexer) peekChar() byte {
-	if l.readPosition >= len(l.input) {
-		return 0
-	} else {
-		return l.input[l.readPosition]
 	}
 }
 
@@ -112,26 +70,35 @@ func newToken(tokenType token.TokenType, ch byte) token.Token {
 	return token.Token{Type: tokenType, Literal: string(ch)}
 }
 
-func isLetter(ch byte) bool {
-	return 'a' <= ch && ch <= 'z' || 'A' <= ch && ch <= 'Z' || ch == '_'
-}
+// func (l *Lexer) peekChar() byte {
+// 	// readPosition의 문자를 byte로 리턴하는 처리.
+// 	if l.readPosition >= len(l.input) {
+// 		return 0
+// 	} else {
+// 		return l.input[l.readPosition]
+// 	}
+// }
 
-func isDigit(ch byte) bool {
-	return '0' <= ch && ch <= '9'
-}
+// func isLetter(ch byte) bool {
+// 	return 'a' <= ch && ch <= 'z' || 'A' <= ch && ch <= 'Z' || ch == '_'
+// }
 
-func (l *Lexer) readIdentifier() string {
-	position := l.position
-	for isLetter(l.ch) {
-		l.readChar()
-	}
-	return l.input[position:l.position]
-}
+// func isDigit(ch byte) bool {
+// 	return '0' <= ch && ch <= '9'
+// }
 
-func (l *Lexer) readNumber() string {
-	position := l.position
-	for isDigit(l.ch) {
-		l.readChar()
-	}
-	return l.input[position:l.position]
-}
+// func (l *Lexer) readIdentifier() string {
+// 	position := l.position
+// 	for isLetter(l.ch) {
+// 		l.readChar()
+// 	}
+// 	return l.input[position:l.position]
+// }
+
+// func (l *Lexer) readNumber() string {
+// 	position := l.position
+// 	for isDigit(l.ch) {
+// 		l.readChar()
+// 	}
+// 	return l.input[position:l.position]
+// }
--- 01/src/monkey/lexer/lexer_test.go
+++ 01/src/monkey/lexer/lexer_test.go
@@ -6,49 +6,45 @@ import (
 	"monkey01/token"
 )
 
-func TestNextToken(t *testing.T) {
-	input := `let five = 5;
-	let ten = 10;
-	let add = fn(x, y) {
-		x + y;
-	};
-	`
+type dbg_token struct {
+	expectedType    token.TokenType
+	expectedLiteral string
+}
 
-	tests := []struct {
-		expectedType    token.TokenType
-		expectedLiteral string
-	}{
-		{token.LET, "let"},
-		{token.IDENT, "five"},
+func TestNextToken(t *testing.T) {
+	input := `=+(){},;`
+
+	// tests := []struct {
+	// 	expectedType    token.TokenType
+	// 	expectedLiteral string
+	// }{
+	// 	{token.ASSIGN, "="},
+	// 	{token.PLUS, "+"},
+	// 	{token.LPAREN, "("},
+	// 	{token.RPAREN, ")"},
+	// 	{token.LBRACE, "{"},
+	// 	{token.RBRACE, "}"},
+	// 	{token.COMMA, ","},
+	// 	{token.SEMICOLON, ";"},
+	// 	{token.EOF, ""},
+	// }
+
+	// input 의 문자, 단어(토큰)과 일치하게 만들어야 테스트가 통과 됨.
+	tests2 := []dbg_token{
 		{token.ASSIGN, "="},
-		{token.INT, "5"},
-		{token.SEMICOLON, ";"},
-		{token.LET, "let"},
-		{token.IDENT, "ten"},
-		{token.ASSIGN, "="},
-		{token.INT, "10"},
-		{token.SEMICOLON, ";"},
-		{token.LET, "let"},
-		{token.IDENT, "add"},
-		{token.ASSIGN, "="},
-		{token.FUNCTION, "fn"},
+		{token.PLUS, "+"},
 		{token.LPAREN, "("},
-		{token.IDENT, "x"},
-		{token.COMMA, ","},
-		{token.IDENT, "y"},
 		{token.RPAREN, ")"},
 		{token.LBRACE, "{"},
-		{token.IDENT, "x"},
-		{token.PLUS, "+"},
-		{token.IDENT, "y"},
-		{token.SEMICOLON, ";"},
 		{token.RBRACE, "}"},
+		{token.COMMA, ","},
 		{token.SEMICOLON, ";"},
+		{token.EOF, ""},
 	}
 
 	l := New(input)
 
-	for i, tt := range tests {
+	for i, tt := range tests2 {
 		tok := l.NextToken()
 
 		if tok.Type != tt.expectedType {
