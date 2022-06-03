01c0fecd179bc40765543289b61bcab44e163327
Scottbang <scottbang.kr@gmail.com>
2022-06-03 15:33:47 +0900

    repl 사용하도록 수정

--- monkey/main.go
+++ monkey/main.go
@@ -1,58 +1,42 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
-	"io"
-	"monkey/lexer"
-	"monkey/token"
+	"monkey/repl"
 	"os"
 	"os/user"
 )
 
-const PROMPT = ">>"
-
 func main() {
-	user, err := user.Current()
+	userObj, err := user.Current()
 	if err != nil {
 		panic(err)
 	}
 
-	fmt.Printf("Hello %s! This is the Monkey programming language!\n", user.Username)
+	fmt.Printf("Hello %s! This is the Monkey programming language!\n", userObj.Username)
 	fmt.Printf("Feel free to type in commands\n")
 
-	tmp := fmt.Sprintf("pwd: %s, Gid: %s, Uid: %s", user.HomeDir, user.Gid, user.Uid)
-	fmt.Fprint(os.Stdout, PROMPT)
+	tmp := fmt.Sprintf("pwd: %s, Gid: %s, Uid: %s\n", userObj.HomeDir, userObj.Gid, userObj.Uid)
 	fmt.Fprintf(os.Stdout, tmp)
 
+	repl.Start(os.Stdin, os.Stdout)
 }
 
-func myback() {
-	user, err := user.Current()
-	if err != nil {
-		panic(err)
-	}
-
-	fmt.Printf("Hello %s! This is the Monkey programming language!\n", user.Username)
-	fmt.Printf("Feel free to type in commands\n")
-	mytest(os.Stdin, os.Stdout)
-}
-
-func mytest(in io.Reader, out io.Writer) {
-	scanner := bufio.NewScanner(in)
-
-	for {
-		fmt.Fprintf(out, PROMPT)
-		scanned := scanner.Scan()
-		if !scanned {
-			return
-		}
-
-		line := scanner.Text()
-		l := lexer.New(line)
-
-		for tok := l.NextToken(); tok.Type != token.EOF; tok = l.NextToken() {
-			fmt.Fprintf(out, "%+v\n", tok)
-		}
-	}
-}
+//func mytest(in io.Reader, out io.Writer) {
+//	scanner := bufio.NewScanner(in)
+//
+//	for {
+//		fmt.Fprintf(out, PROMPT)
+//		scanned := scanner.Scan()
+//		if !scanned {
+//			return
+//		}
+//
+//		line := scanner.Text()
+//		l := lexer.New(line)
+//
+//		for tok := l.NextToken(); tok.Type != token.EOF; tok = l.NextToken() {
+//			fmt.Fprintf(out, "%+v\n", tok)
+//		}
+//	}
+//}
